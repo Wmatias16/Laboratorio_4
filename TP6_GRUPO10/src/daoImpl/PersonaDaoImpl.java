705a688c06ae88c063cac686af22d311bc593fc6
Eduardo Granado <eduardoenriquegranado@gmail.com>
2021-10-18 20:48:26 -0300

    Todas las queries a la base implementadas

--- TP6_GRUPO10/src/daoImpl/PersonaDaoImpl.java
+++ TP6_GRUPO10/src/daoImpl/PersonaDaoImpl.java
@@ -2,7 +2,9 @@ package daoImpl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import dao.IPersonaDao;
@@ -38,25 +40,62 @@ public class PersonaDaoImpl implements IPersonaDao{
 			} catch (SQLException e1) {
 				e1.printStackTrace();
 			}
-		}
-		
-		
+		}		
 		return estado;
 	}
 
 	
 	public boolean update(Persona persona) {
-		return false;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		boolean actualizacionExitosa = false;
+		try {
+			PreparedStatement preparedStatement = conexion.prepareStatement("UPDATE personas SET Nombre = ?, Apellido = ? WHERE Dni = ?");
+			preparedStatement.setString(1, persona.getNombre());
+			preparedStatement.setString(2, persona.getApellido());
+			preparedStatement.setString(3, persona.getDni());
+			if (preparedStatement.executeUpdate() > 0) {
+				conexion.commit();
+				actualizacionExitosa = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return actualizacionExitosa;
 	}
 
 	
 	public List<Persona> getAll() {
-		return null;
+		List<Persona> personas = new ArrayList<>();
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		try {
+			PreparedStatement preparedStatement = conexion.prepareStatement("SELECT Dni, Nombre, Apellido FROM personas");
+			ResultSet resultSet = preparedStatement.executeQuery();
+			while (resultSet.next()) {
+				personas.add(new Persona(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3)));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return personas;
 	}
 
 	
 	public boolean delete(Persona persona) {
-		return false;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		boolean borradoExitoso = false;
+		try {
+			PreparedStatement preparedStatement = conexion.prepareStatement("DELETE FROM personas WHERE Dni = ?");
+			preparedStatement.setString(1, persona.getDni());
+			if (preparedStatement.executeUpdate() > 0) {
+				conexion.commit();
+				borradoExitoso = true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return borradoExitoso;
 	}
+	
+	
 
 }
