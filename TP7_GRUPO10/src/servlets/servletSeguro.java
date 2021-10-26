630f8d5e6a83fdebb63019718b496717ac2b17a6
Eduardo Granado <eduardoenriquegranado@gmail.com>
2021-10-25 21:36:41 -0300

    Servlet ListarSeguros

---
+++ TP7_GRUPO10/src/servlets/servletSeguro.java
@@ -0,0 +1,57 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dominio.Seguro;
+import dominio.SeguroDao;
+
+
+/**
+ * Servlet implementation class servletSeguro
+ */
+@WebServlet("/servletSeguro")
+public class servletSeguro extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public servletSeguro() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("Param")!=null) {
+			//Entra por haber echo click en el hyperlink listar seguros
+			SeguroDao seguroDao = new SeguroDao();
+			List<Seguro> lista= seguroDao.obtenerSeguros();
+			
+			request.setAttribute("listaSeguros", lista);
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/ListarSeguros.jsp");   
+	        rd.forward(request, response);
+			
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
